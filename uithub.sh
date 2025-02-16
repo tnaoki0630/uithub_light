f2241fcb846463f39698c06e45639c9e4b421684
tnaoki0630 <naoki.tsunezawa.s8@dc.tohoku.ac.jp>
2025-02-16 17:49:56 +0900

    sh 内で参照するパスを一般化し、最低限のエラー処理を追加

--- uithub.sh
+++ uithub.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# 実行パスを取得
+exec_path=$(pwd)
+if [ ! -d "$exec_path/.git" ]; then
+  echo "[Error] .git ディレクトリが見つかりません。"
+  echo "        本スクリプトはリポジトリのルートで実行する必要があります。" >&2
+  exit 1 
+fi
+
+# 抽出するファイルリストのパスを取得
+list_path=$(dirname "$0")/src_list.txt
+if [ ! -f "$list_path" ]; then
+  echo "[Error] ${list_path}が見つかりません。"
+  echo "        新しく作成するか、https://github.com/tnaoki0630/uithub_light から clone してください。" >&2
+  exit 1 
+fi
+
 # ファイルツリーを上書き出力
 tree > src.txt
 echo "-------------------------------------" >> src.txt
@@ -8,7 +24,7 @@ echo "" >> src.txt
 # 各パターンに対してマッチするファイルを検索し、重複を除いてループで処理
 while IFS= read -r pattern; do
   find . -type f -name "$pattern" -print0
-done < tools/src_list.txt | sort -zu | while IFS= read -r -d '' file; do
+done < $list_path | sort -zu | while IFS= read -r -d '' file; do
   # ファイルの相対パスをヘッダーとして出力
   echo "-------------------------------------" >> src.txt
   echo "File: $file" >> src.txt
